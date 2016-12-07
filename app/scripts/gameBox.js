4c87d158cd7b3ba7a16074c4a8e7fa95057cc504
Nate Bender <Nate Bender>
2016-12-07 13:47:57 -0500

    Created barebones classes

---
+++ app/scripts/gameBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import $ from 'jquery';
+
+import playerHand from './playerHand.js';
+import table from './table.js';
+import otherPlayers from './otherPlayers.js'
+
+module.exports = React.createClass({
+
+
+
+});
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Redirect, browserHistory } from 'react-router';
 
-//import '../css/base.css';
+import gameBox from './gameBox.js';
+import joinBox from './joinBox.js'
+import '../css/base.css';
 
 ReactDOM.render((
     <Router history={browserHistory}>
-//        <Route path="/" component={CommentBox}/>
-//        <Route path="/:id" component={CommentEdit} />
+        <Route path="/" component={JoinBox}/>
+        <Route path="/:id" component={GameBox} />
     </Router>
 ), document.getElementById('content')
 );
