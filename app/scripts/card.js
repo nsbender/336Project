c5b274085dab1011f3dcf1fab89344a3c401035a
Christiaan Hazlett <christiaanhazlett@gmail.com>
2016-12-07 14:21:06 -0500

    Fixed the images directory issue

---
+++ app/scripts/card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import $ from 'jquery';
+
+
+module.exports = React.createClass({
+
+  render: function() {
+      return (
+        // <div>
+        //   <image style="width: 100%;" src="images/cards/red_joker.png"></image>
+        // </div>
+        //
+        //
+        // <div>
+        // <h1>Welcome To PokerStars.net</h1>
+        // <h2>Create a new game, or enter the id of an already existing game in the field below</h2>
+        // </div>
+      );
+  }
+
+});
