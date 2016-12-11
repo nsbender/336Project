dd8e4d3271a6a3976352484b272623ec72ca18a4
Nathaniel Bender <nsbender1@gmail.com>
2016-12-11 11:09:22 -0500

    expirimental gamebox element height

--- app/scripts/gameBox.js
+++ app/scripts/gameBox.js
@@ -6,11 +6,18 @@ import table from './table.js';
 import otherPlayers from './otherPlayers.js'
 
 module.exports = React.createClass({
-
   render: function() {
       return (
         <div>
-        <h1>This is an example game page</h1>
+          <div className="otherPlayers">
+            <h1>Other Players</h1>
+          </div>
+          <div className="table">
+            <h1>Table</h1>
+          </div>
+          <div className="playerHand">
+            <h1>Player Hand</h1>
+          </div>
         </div>
       );
   }
